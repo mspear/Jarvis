d318a2326404d756997aacd6756cdbc51729e685
Michael <mwspear@gmail.com>
2017-01-12 18:20:12 -0600

    say function

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pyttsx
 
 def speak(words):
-	
+	global speechengine
+	speechenginw.say(words)
 def main():
 	global speechengine
 	speechengine = pyttsx('nsss')
+	speak('hello world')
+if __name__ == '__main__':
+	main()
